42179fcd57fff00b947c4ba3bb333d2dbdae3663
Andrew King <pepsiotaku@gmail.com>
2021-03-20 14:47:39 -0400

    preBuild script stuff

--- csbuild_Release.sh
+++ csbuild_Release.sh
@@ -1,1 +1,11 @@
-msbuild /property:Configuration=Release
+#!/bin/bash
+
+if [[ $OSTYPE == 'msys' || $OSTYPE == 'win32' ]]; then
+	PLATFORM=windows
+fi
+
+if [[ $PLATFORM == "windows" ]]; then
+	msbuild.exe /property:Configuration=Release
+else
+	msbuild /property:Configuration=Release
+fi
