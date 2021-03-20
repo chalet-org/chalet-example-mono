f495bd6e17cee9df38a18d9342401201c6a1c20e
Andrew King <pepsiotaku@gmail.com>
2021-03-19 21:11:26 -0400

    look for MSBUILD_PATH on windows to avoid mixing between mono & VS

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,21 @@
 
 int main(/*const int argc, const char* const argv[]*/)
 {
-	const std::string file = "cs-test.dll";
 
 #if defined(MONOSCRIPT_DEBUG)
+	const std::string file = "cs-test-d.dll";
+	#if defined(MONOSCRIPT_WIN32)
+	int buildResult = std::system("msbuild.exe");
+	#else
 	int buildResult = std::system("msbuild");
+	#endif
 	if (buildResult != EXIT_SUCCESS)
 	{
 		std::cerr << "Build failed. See output above." << std::endl;
 		return buildResult;
 	}
+#else
+	const std::string file = "cs-test.dll";
 #endif
 
 	const std::string domain = "monoscript";
